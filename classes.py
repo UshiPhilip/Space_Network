0df5755f67fbebda2a8e93bc8308893f047c1c00
Ushi Philip <ushi7670741@gmail.com>
2026-01-26 14:35:23 +0200

    Took care of errors
    create a function to resend the message till it will arrive the destination

--- classes.py
+++ classes.py
@@ -1,7 +1,23 @@
 from space_network_lib import *
+import time
 
 
 class Satellite(SpaceEntity):
 
     def receive_signal(self, packet: Packet):
         print(f"[{self.name}] Received: {packet}.")
+
+def transmission_attempt(packet):
+    spaceship = SpaceNetwork(level=2)
+    while True:
+        try:
+            spaceship.send(packet)
+            break
+        except TemporalInterferenceError:
+            print("Interference, waiting two seconds...")
+            print("1...")
+            time.sleep(1)
+            print("2...")
+            time.sleep(1)
+        except DataCorruptedError:
+            print("Data corrupted, retrying...")
